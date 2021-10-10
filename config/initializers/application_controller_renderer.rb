d7d20008b48058e61f1cdc20c6ecea9989556339
yutotangot <tangotani.yuto.infratop@gmail.com>
2021-10-10 13:13:01 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '50bcf9bb04119fa9e6652c604624515f5a042f34dc8b79377d0efe60e3f2d0746ff5c9b6bfc9d04b67048593da091ae396cea49fdf863abdf52a8cc479cc2d4a'
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+end
